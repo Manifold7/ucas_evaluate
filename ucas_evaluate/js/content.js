7bdc93faba0b0e6f57ab980785f93a52fc2aa633
王大可 <37853443+Manifold7@users.noreply.github.com>
2020-07-01 13:59:33 +0800

    更新到2020夏季学期

--- ucas_evaluate/js/content.js
+++ ucas_evaluate/js/content.js
@@ -1,10 +1,10 @@
-for (var i=34;i<67;i++)
+for (var i=133;i<160;i++)
 { 
     if($("input[name='item_"+i+"']")[0])
 	{
 		$("input[name='item_"+i+"']")[0].checked=true;
 	}
 }
-document.getElementById('item_43').value = "老师风趣幽默的讲课风格和扎实的课堂内容。";
-document.getElementById('item_44').value = "老师的讲解满足我的期望，没有意见。";
+document.getElementById('item_145').value = "老师风趣幽默的讲课风格和扎实的课堂内容。";
+document.getElementById('item_146').value = "老师的讲解满足我的期望，没有意见。";
 document.getElementById("sb1").click();
